fed1f1a784f6e8cdea495ed62931ee6548eccaf2
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-11-05 06:42:05 +0000

    photos progress done except for forms

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,21 @@
 class PhotosController < ApplicationController
 
 def index
-  
+    matching_photos = Photo.all
+  @list_of_photos = matching_photos.order({:created_at =>:desc})
+
     render({ :template =>"photo_templates/index.html.erb"})
   end
 
 def show
+  the_id = params.fetch(:photo_id)
+
+  matching_photo = Photo.where({:id => the_id})
+  @the_photo = matching_photo.at(0)
+
+  matching_comments = Comment.where({:photo_id => the_id})
+  @list_of_the_comments = matching_comments.order({:created_at=>:desc})
+  
     render({ :template =>"photo_templates/show.html.erb"})
   end
 end
